b8238466573ae8634d2ba5d3e6ab9764fcf411ed
Rangerson Clemente <rangerson14@outlook.com>
2023-03-09 18:08:49 -0300

    atualizando...

--- Animacao com Rotacao/coding.js
+++ Animacao com Rotacao/coding.js
@@ -1,6 +1,5 @@
-
-const ler = document.querySelector("#image")
+/*const ler = document.querySelector("#image")
 ler.classList.add("image_with_animation")
 ler.style.transform = "rotate(infinite)"
 ler.style.position = "relative"
-ler.style.animation = "loading 3s linear infinite"
+ler.style.animation = "loading 3s linear infinite"*/
