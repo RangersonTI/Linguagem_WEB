430508640f76984f77b15709272c308284105872
Rangerson Clemente <rangerson14@outlook.com>
2023-03-10 00:22:57 -0300

    atualizando e testando...

--- Animacao com Rotacao/coding.js
+++ Animacao com Rotacao/coding.js
@@ -1,5 +1,19 @@
-const ler = document.querySelector("#image")
+/*const ler = document.querySelector("#image")
 ler.classList.add("image_with_animation")
 ler.style.transform = "rotate(infinite)"
 ler.style.position = "relative"
-ler.style.animation = "loading 3s linear infinite"
+ler.style.animation = "loading 3s linear infinite"*/
+
+
+/*document.getElementById('#image_with_animation').addEventListener('click', function(){
+    this.style.animation = 'none';
+    this.style.webkitAnimation = 'none'; // adicionado para compatibilidade com alguns navegadores
+    this.style.mozAnimation = 'none'; // adicionado para compatibilidade com alguns navegadores
+    this.style.msAnimation = 'none'; // adicionado para compatibilidade com alguns navegadores
+    this.style.oAnimation = 'none'; // adicionado para compatibilidade com alguns navegadores
+    this.style.animation = 'none'; // adicionado por último para garantir que seja aplicado em todos os navegadores
+    this.style.animationPlayState = 'paused'; // adicionado para pausar a animação em vez de interrompê-la abruptamente
+  });*/
+('#image_with_animation').click(function(){
+    (this).stop().css('animation','none');
+  });
