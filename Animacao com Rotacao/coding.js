c300b2e4b72a717f1ea1d9a887ac1f8287382fc5
Rangerson Clemente <rangerson14@outlook.com>
2023-03-10 23:33:30 -0300

    animacao concluida

--- Animacao com Rotacao/coding.js
+++ Animacao com Rotacao/coding.js
@@ -13,7 +13,18 @@ ler.style.animation = "loading 3s linear infinite"*/
     this.style.oAnimation = 'none'; // adicionado para compatibilidade com alguns navegadores
     this.style.animation = 'none'; // adicionado por último para garantir que seja aplicado em todos os navegadores
     this.style.animationPlayState = 'paused'; // adicionado para pausar a animação em vez de interrompê-la abruptamente
-  });*/
+});
 ('#image_with_animation').click(function(){
     (this).stop().css('animation','none');
-  });
+});*/
+
+
+const elementoComAnimacao = document.querySelector('.image_with_animation');
+
+elementoComAnimacao.addEventListener('click', function() {
+  if (this.classList.contains('image_with_animation')) {
+    this.classList.remove('image_with_animation');
+  } else {
+    this.classList.add('image_with_animation');
+  }
+});
