ccc33ac230c838ba1da37177b8ce27e4d1ebc4ea
Rangerson Clemente <rangerson14@outlook.com>
2023-03-13 19:42:57 -0300

    atualizado

--- Animacao com Rotacao/coding.js
+++ Animacao com Rotacao/coding.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const elementoComAnimacao = document.querySelector('.image_with_animation');
 
 elementoComAnimacao.addEventListener('click', function() {
@@ -8,31 +7,3 @@ elementoComAnimacao.addEventListener('click', function() {
     this.classList.add('image_with_animation');
   }
 });
-=======
-/*const ler = document.querySelector("#image_with_animation")
-ler.classList.add("image_with_animation")
-ler.style.transform = "rotate(infinite)"
-ler.style.position = "relative"
-ler.style.animation = "loading 3s linear infinite"*/
-
-
-/*document.getElementById('#image_with_animation').addEventListener('click', function(){
-    this.style.animation = 'none';
-    this.style.webkitAnimation = 'none'; // adicionado para compatibilidade com alguns navegadores
-    this.style.mozAnimation = 'none'; // adicionado para compatibilidade com alguns navegadores
-    this.style.msAnimation = 'none'; // adicionado para compatibilidade com alguns navegadores
-    this.style.oAnimation = 'none'; // adicionado para compatibilidade com alguns navegadores
-    this.style.animation = 'none'; // adicionado por último para garantir que seja aplicado em todos os navegadores
-    this.style.animationPlayState = 'paused'; // adicionado para pausar a animação em vez de interrompê-la abruptamente
-});*/
-
-  
-const ler = document.querySelector("#image_with_animation")
-ler.classList.add("#image_with_animation")
-ler.style.transform = "rotate(infinite)"
-ler.style.position = "relative"
-ler.style.animation = "loading 3s linear infinite"
-(ler).click(function(){
-    ler.stop().css('animation','none');
-});
->>>>>>> Stashed changes
