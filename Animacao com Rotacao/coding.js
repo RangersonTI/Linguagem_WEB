2254f95bb02c3ee12324f783768287dedc133aef
Rangerson Clemente <rangerson14@outlook.com>
2023-03-13 20:10:54 -0300

    atualizado

--- Animacao com Rotacao/coding.js
+++ Animacao com Rotacao/coding.js
@@ -1,9 +1,9 @@
 const elementoComAnimacao = document.querySelector('.image_with_animation');
 
-elementoComAnimacao.addEventListener('click', function() {
-  if (this.classList.contains('image_with_animation')) {
+elementoComAnimacao.addEventListener('click',function(){
+  if (this.classList.contains('image_with_animation')){
     this.classList.remove('image_with_animation');
-  } else {
+  }else{
     this.classList.add('image_with_animation');
   }
 });
